fafdad4231709cf0839ae86845f0a19d750f3605
doraa628 <doratsubame@gmail.com>
2026-04-14 15:13:57 +0800

    Update demonstration of graph.py
    updated graph to no import needed and added algorithm required(DFS)

--- Task2/demonstration of graph.py
+++ Task2/demonstration of graph.py
@@ -1,26 +1,40 @@
-import networkx as nx 
-class LibraryGraph:
-    def init(self):
-        self.graph = nx.DiGraph() ##create a graph
-    def reader(self, reader_name):
-        self.graph.add_node(reader_name,type='reader', reader_name=reader_name) ##create a node "reader" and set the type to 'reader' add attribute to seperate reader and book
-    def book(self, book_name):
-        self.graph.add_node(book_name,type='book',book_name=book_name) ##same as reader one but become book
-    def borrow(self, reader_name, book_name, end_day):
-        self.graph.add_edge(reader_name, book_name, end_day=end_day, returnday=None,##return day not yet firmed
-                            status='borrowed') ##set book borrowed by reader and set the due day
-    def bookreturn(self, reader_name, book_name, return_day):##reader returning book
-        self.graph[reader_name][book_name]['return_day']='returned'##make the book returned and set the return day
-    def borrowing(self, reader_name):
-        borrow = [] ##ccreate a borrowed list
-        for book in self.graph.successors(reader_name):
-            edge_data=self.graph[reader_name][book] ##to get all borrowed book from the reader
-            if edge_data.get('status')=='borrowed':
-                borrow.append((book,edge_data['end_day'])) ## if book borrowed add in the list
-        return borrow
-lib = LibraryGraph()
-lib.reader('Leon')
-lib.book('awa')
+class Graph:
+    def __init__(self):
+        self.adj_list={}
+    def add_node(self,node):
+        self.adj_list[node]=[]
+    def add_edge(self, node1, node2):
+        self.add_node(node1)
+        self.add_node(node2)
+        self.adj_list[node1].append(node2)
+        self.adj_list[node2].append(node1)
+    def get_related(self,node):
+        return self.adj_list.get(node,[])
+    def print_graph(self):
+        for node, related in self.adj_list.items():
+            print(node+"->"+str(related))
+    ##building drawing graph and get graph info
+    ##========================================================##
+    ##DFS algorithm
+    def dfs(self,start_node):
+        stack=[start_node]
+        searched=set()
+        while stack:
+            now=stack.pop()
+            if now not in searched:
+                searched.add(now)  ## set searched
+                for related in self.get_related(now):
+                    if related not in searched:
+                        stack.append(related)
+                        ##if related add in list
+        return searched
 
-lib.borrow('Leon','awa','20266-02-31')
-print("borrowed: ",lib.borrowing)
+g = Graph()
+g.add_edge("amy", "alice wonderland")
+g.add_edge("jimmy", "dictionary")
+g.add_edge("mimi", "harry potter")
+g.add_edge("mami", "peppa pig")
+
+g.print_graph()
+result = g.dfs("amy")
+print(result)
